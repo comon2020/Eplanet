360d4efc1f4d755866817128f4b82b11d528410f
Baekjiyeon <aa01047353119@gmail.com>
2020-02-18 17:28:01 +0900

    지연: 프로젝트생성. 첫메인페이지 배경음악_화면잠금버튼 누를때 멈추도록해야함.

---
+++ app/src/main/java/com/example/common/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 지연 : 배경음악 객체
+    private static MediaPlayer mp;
+    // 지연 : 배경음악 설정
+    // 지연 : 홈버튼 누를때 음악 종료
+    @Override
+    protected void onUserLeaveHint(){
+        mp.pause();
+        super.onUserLeaveHint();
+    }
+    @Override
+    public void onResume(){
+        mp.start();
+        super.onResume();
+    }
+    @Override
+    public void onDestroy(){
+        mp.stop();
+        super.onDestroy();
+    }
+    @Override
+    public void onBackPressed(){
+        mp.stop();
+        super.onBackPressed();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 지연 : 타이틀 바 없애기
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        // 지연 : 배경음악시작
+        mp = MediaPlayer.create(this, R.raw.main_music);
+        mp.setLooping(true); // 반복여부
+        mp.start();
+
+        // 두 버튼 연결
+        Button main_btn1 = (Button) findViewById(R.id.main_btn1);
+        Button main_btn2 = (Button) findViewById(R.id.main_btn2);
+
+        // 새 게임 버튼 누르면
+        main_btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "액티비티 전환", Toast.LENGTH_LONG).show();
+                // 액티비티 전환 코드
+                Intent intent = new Intent(getApplicationContext(), NewGameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 불러오기 버튼 누르면
+        main_btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "액티비티 전환", Toast.LENGTH_LONG).show();
+                // 액티비티 전환 코드
+                Intent intent = new Intent(getApplicationContext(), NewGameActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/common/NewGameActivity.java
@@ -0,0 +1,22 @@
+package com.example.common;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewGameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_newgame);
+    }
+
+
+}
